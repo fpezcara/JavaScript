3dd1eb3c3f8817b47958a6a5c19186901cfa3ed3
fpezcara <fpezcara@gmail.com>
2019-10-24 20:36:53 +0100

    Primera tarea J

---
+++ tarea.js
@@ -0,0 +1,30 @@
+// 1. Declarar variable saludo y asignarle el valor "Hola, chicas". Mostrarla en la consola.
+let saludo = "Hola, chicas.";
+
+console.log(saludo);
+
+// 2. Mostrar el siguiente mensaje en la consola sin asignarle ninguna variable. "Soy un string variable"
+console.log("Soy un string variable");
+
+// 3. Declarar una variable nombre con tu nombre. Declarar una variable bienvenida con el texto bienvenida. Mostrar en la consola el mensaje completo ("Hola Ana")
+let nombre = "Florencia";
+
+let bienvenida = "Hola";
+
+console.log(bienvenida + " " + nombre);
+
+// 4. Declarar una variable con el valor Jay's. Declarar una variable con el valor "es el mejor"local"". Mostrar en consola "Jay's es el mejor local"
+let restaurante = `Jay's`
+
+let comentario = `es el "mejor" local`
+
+console.log(restaurante + " " + comentario)
+
+// 5. Declarar una variable primer texto con el valor: este texto tiene "comillas dobles". Declarar una variable segundoTexto con el valor: este texto tiene 'comillas simples'. Mostrar en consola ambos valores
+let primerTexto = `Este texto tiene "comillas dobles" `;
+
+let segundoTexto = `Este texto tiene 'comillas simples' `;
+
+console.log(primerTexto);
+
+console.log(segundoTexto);
